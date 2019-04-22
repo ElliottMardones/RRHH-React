64ffd45b12a92835c02714b757d84068c80edb84
ElliottMardones <elliottmardones@gmail.com>
2019-04-22 18:00:34 -0400

    Init

--- rrhh/src/App.js
+++ rrhh/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Dejame editar el readme
         </p>
         <a
           className="App-link"
