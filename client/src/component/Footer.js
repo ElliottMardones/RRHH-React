7dc4581e9e47dbe131f1d1c09494a72bbb40851f
ItsmeSonny <xAleeh@live.cl>
2019-05-01 16:09:31 -0400

    Footer
    Creacion del footer y agregado en el app.cs el .footer

---
+++ client/src/component/Footer.js
@@ -0,0 +1,35 @@
+import React,{ Component } from 'react';
+import { 
+	Container,
+	Col,
+	Row
+} from 'reactstrap';
+
+class Footer extends Component{
+	render() {
+        return (
+            <div className="footer">
+            	<Container fluid>
+            		<Container>
+	                	<Row >
+	                		<Col md={{size:6,offset:5}} xs={{size:10, offset:3}} lg={{size:6,offset:4}}>
+		                		<h4 >Contactenos</h4>
+					            <i className="fa fa-phone fa-lg"></i>: +56974861243<br/>
+					            <i className="fa fa-envelope fa-lg"></i>:UCT@gmail.com 
+				                <h5> Siguenos en:</h5>
+					            <i className="fa fa-facebook fa-lg"></i>: Los Topoyiyo<br/>
+					            <i className="fa fa-twitter fa-lg"></i>: Los Topoyiyos twitter<br/>
+					        </Col>
+				        </Row>
+	                </Container>
+                	<hr className="my-2" />
+                	<Row>
+						<Col md={{size:6,offset:5}} xs={{size:10, offset:2}} lg={{size:6,offset:5}}><p>©Copyright: UCT Informatica</p></Col>
+                	</Row>
+				</Container>
+				<br/>
+			</div>
+        );
+    }
+}
+export default Footer;
