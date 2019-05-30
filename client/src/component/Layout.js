25c9756ae0573b0c1066cf2b34426091c1972d98
Iván Topp Sandoval <itopp2015@alu.uct.cl>
2019-05-29 21:21:05 -0400

    Update From Master(?

---
+++ client/src/component/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Row, Col ,Badge, Jumbotron} from 'reactstrap';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+      <Row>
+        <Col xs="auto">El trabajo sus condiciones y el tipo de empleo, repercuten en la protección e impactan en el sistema previsional.l</Col>
+        <Col xs="auto">El trabajo sus condiciones y el tipo de empleo, repercuten en la protección e impactan en el sistema previsional.l</Col>
+        <Col xs="auto">El trabajo sus condiciones y el tipo de empleo, repercuten en la protección e impactan en el sistema previsional.l</Col>
+        <Col xs="auto">El trabajo sus condiciones y el tipo de empleo, repercuten en la protección e impactan en el sistema previsional.l</Col>
+      </Row>
+      </Jumbotron>
+    </div>
+    );
+  }
+}
